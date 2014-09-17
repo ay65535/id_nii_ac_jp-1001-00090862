f2445c8db44ee2e37aa5e20727ab72db6560fc76
ay65535 <ay65535@icloud.com>
2014-09-17 16:43:43 +0900

    Create PyCharm project

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='id_nii_ac_jp-1001-00090862',
+    version='0.1.0',
+    packages=['id_nii_ac_jp-1001-00090862'],
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description=''
+)
